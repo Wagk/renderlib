95ba68cf59fd9fd0c8d9e7da17b8e40f3ffbafba
pang.t <polkafreaku@gmail.com>
2016-07-27 18:58:26 +0800

    smallest window

--- renderlib/main.cpp
+++ renderlib/main.cpp
@@ -21,29 +21,34 @@
 
 #include "imgui_impl_glfw_gl3.h"
 
+
+GLFWwindow* g_context = nullptr;
+
+
 static void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error %d: %s\n", error, description);
 }
 
-int main(int, char**)
+static void sys_init()
 {
 	// Setup window
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit())
-		return 1;
+		throw std::runtime_error("GLFW failed to init!");
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #if __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
-	glfwMakeContextCurrent(window);
+	g_context = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
+	glfwMakeContextCurrent(g_context);
 	glewInit();
 
 	// Setup ImGui binding
-	ImGui_ImplGlfwGL3_Init(window, true);
+	ImGui_ImplGlfwGL3_Init(g_context, true);
 
 	// Load Fonts
 	// (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
@@ -54,53 +59,64 @@ int main(int, char**)
 	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
 	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
 	//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+}
+
+int main(int, char**)
+{
+
+	sys_init();
 
 	bool show_test_window = true;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImColor(114, 144, 154);
 
 	// Main loop
-	while (!glfwWindowShouldClose(window))
+	while (!glfwWindowShouldClose(g_context))
 	{
 		glfwPollEvents();
 		ImGui_ImplGlfwGL3_NewFrame();
 
-		// 1. Show a simple window
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
-		{
-			static float f = 0.0f;
-			ImGui::Text("Hello, world!");
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-			ImGui::ColorEdit3("clear color", (float*)&clear_color);
-			if (ImGui::Button("Test Window")) show_test_window ^= 1;
-			if (ImGui::Button("Another Window")) show_another_window ^= 1;
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
+		//// 1. Show a simple window
+		//// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+		//{
+		//	static float f = 0.0f;
+		//	ImGui::Text("Hello, world!");
+		//	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+		//	ImGui::ColorEdit3("clear color", (float*)&clear_color);
+		//	if (ImGui::Button("Test Window")) show_test_window ^= 1;
+		//	if (ImGui::Button("Another Window")) show_another_window ^= 1;
+		//	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		//}
 
-		// 2. Show another simple window, this time using an explicit Begin/End pair
-		if (show_another_window)
-		{
-			ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello");
-			ImGui::End();
-		}
+		//// 2. Show another simple window, this time using an explicit Begin/End pair
+		//if (show_another_window)
+		//{
+		//	ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
+		//	ImGui::Begin("Another Window", &show_another_window);
+		//	ImGui::Text("Hello");
+		//	ImGui::End();
+		//}
 
-		// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-		if (show_test_window)
-		{
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
-			ImGui::ShowTestWindow(&show_test_window);
-		}
+		//// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+		//if (show_test_window)
+		//{
+		//	ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+		//	ImGui::ShowTestWindow(&show_test_window);
+		//}
+
+		bool math_window = false;
+		ImGui::Begin("Test", &math_window);
+			//stuff inside
+		ImGui::End();
 
 		// Rendering
 		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glfwGetFramebufferSize(g_context, &display_w, &display_h);
 		glViewport(0, 0, display_w, display_h);
 		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui::Render();
-		glfwSwapBuffers(window);
+		glfwSwapBuffers(g_context);
 	}
 
 	// Cleanup
