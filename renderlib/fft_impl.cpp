311af942169f2120366ca1b14cf1bc57619ea5e5
asm <alphastickmania@hotmail.com>
2016-08-07 19:44:14 +0800

    Added untested code from porting spectrum display. Added RealFFT from Audacity for testing.

--- renderlib/fft_impl.cpp
+++ renderlib/fft_impl.cpp
@@ -1,6 +1,10 @@
 #include "fft_impl.h"
 #include <cmath>
 
+#ifndef max
+#define max(a, b) a > b ? a : b
+#endif
+
 // Local functions
 bool IsPowerOfTwo(int x);
 int BitsRequired(int pow2val);
@@ -10,7 +14,16 @@ int ReverseBits(int index, int bitCount);
 int** SignalAnalyst::st_FFTBitTable = nullptr;
 const int SignalAnalyst::st_FFTBitTableLimit = 16;
 
-double st_AngleNumerator = 0.0;
+double SignalAnalyst::st_AngleNumerator = 0.0;
+
+WindowFuncPtr SignalAnalyst::st_WinFunc = SignalAnalyst::WFInitialise;
+int SignalAnalyst::st_WinSize = 0;
+float* SignalAnalyst::st_Data = nullptr;
+int SignalAnalyst::st_DataLength = 0;
+float SignalAnalyst::st_Rate = 0.0f;
+
+std::vector<float> SignalAnalyst::st_Processed;
+float SignalAnalyst::st_YMin = 0.0f, SignalAnalyst::st_YMax = 0.0f;
 
 void SignalAnalyst::Init()
 {
@@ -59,13 +72,61 @@ void SignalAnalyst::IFFT(int n, float* realIn, float* imagIn, float* realOut, fl
 	}
 }
 
+void SignalAnalyst::RealFFT(int n, float* realIn, float* realOut, float* imagOut)
+{
+	int half = n / 2;
+	float theta = static_cast<float>(M_PI / half);
+
+	float* tempReal = new float[half];
+	float* tempImag = new float[half];
+
+	for (int i = 0; i < half; ++i)
+	{
+		tempReal[i] = realIn[2 * i];
+		tempImag[i] = realIn[2 * i + 1];
+	}
+
+	FFT(half, tempReal, tempImag, realOut, imagOut);
+
+	float wtemp = sin(0.5f * theta);
+	float wpr = -2.0f * wtemp * wtemp;
+	float wpi = -1.0f * sin(theta);
+	float wr = 1.0f + wpr;
+	float wi = wpi;
+
+	for (int i = 1; i < half / 2; i++) {
+
+		int i3 = half - i;
+
+		float h1r = 0.5f * (realOut[i] + realOut[i3]);
+		float h1i = 0.5f * (imagOut[i] - imagOut[i3]);
+		float h2r = 0.5f * (imagOut[i] + imagOut[i3]);
+		float h2i = -0.5f * (realOut[i] - realOut[i3]);
+
+		realOut[i] = h1r + wr * h2r - wi * h2i;
+		imagOut[i] = h1i + wr * h2i + wi * h2r;
+		realOut[i3] = h1r - wr * h2r + wi * h2i;
+		imagOut[i3] = -h1i + wr * h2i + wi * h2r;
+
+		wr = (wtemp = wr) * wpr - wi * wpi + wr;
+		wi = wi * wpr + wtemp * wpi + wi;
+	}
+
+	wtemp = realOut[0];
+	realOut[0] += imagOut[0];
+	imagOut[0] = wtemp - imagOut[0];
+
+	delete[] tempReal;
+	delete[] tempImag;
+}
+
 void SignalAnalyst::PowerSpectrum(int n, float* in, float* out)
 {
 	int half = n / 2;
 	int quarter = half / 2;
 	int i;
 
-	float theta = M_PI / half;
+	float theta = static_cast<float>(M_PI) / half;
 
 	float *tmpReal = new float[half];
 	float *tmpImag = new float[half];
@@ -81,9 +142,9 @@ void SignalAnalyst::PowerSpectrum(int n, float* in, float* out)
 
 	float wtemp = float(sin(0.5 * theta));
 
-	float wpr = -2.0 * wtemp * wtemp;
-	float wpi = -1.0 * float(sin(theta));
-	float wr = 1.0 + wpr;
+	float wpr = -2.0f * wtemp * wtemp;
+	float wpi = -1.0f * float(sin(theta));
+	float wr = 1.0f + wpr;
 	float wi = wpi;
 
 	int i3;
@@ -94,10 +155,10 @@ void SignalAnalyst::PowerSpectrum(int n, float* in, float* out)
 
 		i3 = half - i;
 
-		h1r = 0.5 * (realOut[i] + realOut[i3]);
-		h1i = 0.5 * (imagOut[i] - imagOut[i3]);
-		h2r = 0.5 * (imagOut[i] + imagOut[i3]);
-		h2i = -0.5 * (realOut[i] - realOut[i3]);
+		h1r = 0.5f * (realOut[i] + realOut[i3]);
+		h1i = 0.5f * (imagOut[i] - imagOut[i3]);
+		h2r = 0.5f * (imagOut[i] + imagOut[i3]);
+		h2i = -0.5f * (realOut[i] - realOut[i3]);
 
 		rt = h1r + wr * h2r - wi * h2i;
 		it = h1i + wr * h2i + wi * h2r;
@@ -125,6 +186,192 @@ void SignalAnalyst::PowerSpectrum(int n, float* in, float* out)
 	delete[] tmpImag;
 	delete[] realOut;
 	delete[] imagOut;
+}
+
+WindowFuncPtr SignalAnalyst::GetWindowFunction(const WindowTypeFunctions& type)
+{
+	switch (type)
+	{
+	case WTF_RECTANGULAR:
+		return WFRectangular;
+
+	case WTF_HANNING:
+		return WFHanning;
+
+	case WTF_BLACKMAN:
+		return WFBlackman;
+	}
+
+	return WFInitialise;
+}
+
+void SignalAnalyst::SelectWindowFunction(const WindowTypeFunctions& type)
+{
+	st_WinFunc = GetWindowFunction(type);
+}
+
+void SignalAnalyst::SetWindowSize(int size)
+{
+	st_WinSize = size;
+}
+
+void SignalAnalyst::SetDataReference(float* data, int dataLen)
+{
+	st_Data = data;
+	st_DataLength = dataLen;
+}
+
+void SignalAnalyst::SetDataRate(float rate)
+{
+	if (rate > 0.0f)
+		st_Rate = rate;
+}
+
+float SignalAnalyst::CubicInterpolate(float y0, float y1, float y2, float y3, float x)
+{
+	float a, b, c, d;
+
+	a = y0 / -6.0f + y1 / 2.0f - y2 / 2.0f + y3 / 6.0f;
+	b = y0 - 5.0f * y1 / 2.0f + 2.0f * y2 - y3 / 2.0f;
+	c = -11.0f * y0 / 6.0f + 3.0f * y1 - 3.0f * y2 / 2.0f + y3 / 3.0f;
+	d = y0;
+
+	float xx = x * x;
+	float xxx = xx * x;
+
+	return (a * xxx + b * xx + c * x + d);
+}
+
+void SignalAnalyst::ComputePlot()
+{
+	if (!st_DataLength ||
+		st_WinSize < 32 ||
+		st_WinSize > 65536 ||
+		st_DataLength < st_WinSize)
+	{
+		return;
+	}
+
+	int half = st_WinSize / 2;
+	st_Processed.resize(st_WinSize, 0.0f);
+
+	float *in = new float[st_WinSize];
+	float *out = new float[st_WinSize];
+	float *win = new float[st_WinSize];
+
+	WFInitialise(st_WinSize, win);
+	st_WinFunc(st_WinSize, win);
+
+	// Compute window scaling (0 dB for amplitude of 1.0)
+	double wss = 0.0;
+	for (int i = 0; i < st_WinSize; ++i)
+		wss += win[i];
+
+	wss = (wss > 0.0) ? 4.0 / (wss * wss) : 1.0;
+
+	int start = 0;
+	int windows = 0;
+
+	while (start + st_WinSize <= st_DataLength)
+	{
+		for (int i = 0; i < st_WinSize; ++i)
+			in[i] = win[i] * st_Data[start + i];
+
+		PowerSpectrum(st_WinSize, in, out);
+
+		for (int i = 0; i < half; ++i)
+			st_Processed[i] += out[i];
+
+		start += half;
+		++windows;
+	}
+
+	st_YMin = 1000000.f;
+	st_YMax = -1000000.f;
+
+	const double scale = wss / static_cast<double>(windows);
+
+	for (int i = 0; i < half; ++i)
+	{
+		st_Processed[i] = 10.0f * log10(st_Processed[i] * static_cast<float>(scale));
+
+		if (st_Processed[i] > st_YMax)
+			st_YMax = st_Processed[i];
+		else if (st_Processed[i] < st_YMin)
+			st_YMin = st_Processed[i];
+	}
+
+	delete[] in;
+	delete[] out;
+	delete[] win;
+}
+
+std::vector<float>& SignalAnalyst::GetProcessed()
+{
+	return st_Processed;
+}
+
+float* SignalAnalyst::GetProcessedRaw()
+{
+	return st_Processed.data();
+}
+
+int SignalAnalyst::GetProcessedSize()
+{
+	return static_cast<int>(st_Processed.size()) / 2;
+}
+
+float SignalAnalyst::GetProcessedValue(float freq0, float freq1)
+{
+	float value = NAN;
+
+	if (st_Rate <= 0.0f || st_Processed.empty())
+		return value;
+
+	float bin0 = freq0 * st_WinSize / st_Rate;
+	float bin1 = freq1 * st_WinSize / st_Rate;
+	float binwidth = bin1 - bin0;
+
+	value = 0.0f;
+
+	if (binwidth < 1.0f)
+	{
+		float binmid = (bin0 + bin1) / 2.0f;
+		int ibin = int(binmid) - 1;
+		if (ibin < 1)
+			ibin = 1;
+		if (ibin >= GetProcessedSize() - 3)
+			ibin = max(0, GetProcessedSize() - 4);
+
+		value = CubicInterpolate(st_Processed[ibin],
+								 st_Processed[ibin + 1],
+								 st_Processed[ibin + 2],
+								 st_Processed[ibin + 3],
+								 binmid - ibin);
+
+	}
+	else
+	{
+		if (bin0 < 0.0f)
+			bin0 = 0.0f;
+		if (bin1 >= GetProcessedSize())
+			bin1 = GetProcessedSize() - 1.0f;
+
+		if (int(bin1) > int(bin0))
+			value += st_Processed[int(bin0)] * (int(bin0) + 1 - bin0);
+
+		bin0 = 1.0f + int(bin0);
+
+		while (bin0 < int(bin1)) {
+			value += st_Processed[int(bin0)];
+			bin0 += 1.0f;
+		}
+		value += st_Processed[int(bin1)] * (bin1 - int(bin1));
+
+		value /= binwidth;
+	}
+
+	return value;
 }
 
 bool SignalAnalyst::TemplateFFT(int n, double fullAngle, float* realIn, float* imagIn, float* realOut, float* imagOut)
@@ -183,11 +430,11 @@ bool SignalAnalyst::TemplateFFT(int n, double fullAngle, float* realIn, float* i
 				double tr = ar0 * realOut[k] - ai0 * imagOut[k];
 				double ti = ar0 * imagOut[k] + ai0 * realOut[k];
 
-				realOut[k] = realOut[j] - tr;
-				imagOut[k] = imagOut[j] - ti;
+				realOut[k] = static_cast<float>(realOut[j] - tr);
+				imagOut[k] = static_cast<float>(imagOut[j] - ti);
 
-				realOut[j] += tr;
-				imagOut[j] += ti;
+				realOut[j] += static_cast<float>(tr);
+				imagOut[j] += static_cast<float>(ti);
 
 				++j;
 			}
@@ -205,6 +452,36 @@ int SignalAnalyst::FastReverseBit(int i, int bitCount)
 		return st_FFTBitTable[bitCount - 1][i];
 
 	return ReverseBits(i, bitCount);
+}
+
+void SignalAnalyst::WFInitialise(int size, float* window)
+{
+	for (int i = 0; i < size; ++i)
+		window[i] = 1.0f;
+}
+
+void SignalAnalyst::WFRectangular(int size, float* window)
+{
+	// Literally do nothing
+}
+
+void SignalAnalyst::WFHanning(int size, float* window)
+{
+	const double multiplier = M_TAU / size;
+	static const double coeff0 = 0.5, coeff1 = -0.5;
+
+	for (int i = 0; i < size; ++i)
+		window[i] *= static_cast<float>(coeff0 + coeff1 * cos(i * multiplier));
+}
+
+void SignalAnalyst::WFBlackman(int size, float* window)
+{
+	const double multiplier = M_TAU / size;
+	const double multiplier2 = 2.0 * multiplier;
+	static const double coeff0 = 0.42, coeff1 = -0.5, coeff2 = 0.08;
+
+	for (int i = 0; i < size; ++i)
+		window[i] *= static_cast<float>(coeff0 + coeff1 * cos(i * multiplier) + coeff2 * cos(i * multiplier2));
 }
 
 // Local functions definition
--- renderlib/fft_impl.h
+++ renderlib/fft_impl.h
@@ -1,12 +1,24 @@
 #pragma once
 // FFT code as done in Audacity
+#include <vector>
+
 #ifndef M_PI
 #define	M_PI		3.14159265358979323846  /* pi */
 #endif
 
 #ifndef M_TAU
-#define	M_TAU		M_PI * 2.0  /* pi */
+#define	M_TAU		M_PI * 2.0
 #endif
+
+typedef void (*WindowFuncPtr)(int, float*);
+
+enum WindowTypeFunctions
+{
+	WTF_RECTANGULAR,
+	WTF_HANNING,
+	WTF_BLACKMAN,
+	WTF_COUNT
+};
 
 class SignalAnalyst
 {
@@ -17,16 +29,50 @@ public:
 	static void FFT(int n, float* realIn, float* imagIn, float* realOut, float* imagOut);
 	static void IFFT(int n, float* realIn, float* imagIn, float* realOut, float* imagOut);
 
+	static void RealFFT(int n, float* realIn, float* realOut, float* imagOut);
+
 	static void PowerSpectrum(int n, float* in, float* out);
+
+	static WindowFuncPtr GetWindowFunction(const WindowTypeFunctions& type);
+
+	// Analysis related
+	static void SelectWindowFunction(const WindowTypeFunctions& type);
+	static void SetWindowSize(int size);
+	static void SetDataReference(float* data, int dataLen);
+	static void SetDataRate(float rate);
+
+	static float CubicInterpolate(float y0, float y1, float y2, float y3, float x);
+	static void ComputePlot();
+
+	static std::vector<float>& GetProcessed();
+	static float* GetProcessedRaw();
+	static int GetProcessedSize();
+	static float GetProcessedValue(float freq0, float freq1);
 
 protected:
 	static bool TemplateFFT(int n, double fullAngle, float* realIn, float* imagIn, float* realOut, float* imagOut);
 
 private:
+	// FFT related
 	static int** st_FFTBitTable;
 	static const int st_FFTBitTableLimit;
 
 	static double st_AngleNumerator;
 
 	static int FastReverseBit(int i, int bitCount);
+
+	static void WFInitialise(int size, float* window);
+	static void WFRectangular(int size, float* window);
+	static void WFHanning(int size, float* window);
+	static void WFBlackman(int size, float* window);
+
+	// Analysis related
+	static WindowFuncPtr st_WinFunc;
+	static int st_WinSize;
+	static float* st_Data;
+	static int st_DataLength;
+	static float st_Rate;
+
+	static std::vector<float> st_Processed;
+	static float st_YMin, st_YMax;
 };
